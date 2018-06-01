a8c21da42eed25168ebdb4a29a1b86051337d9d2
fyliu <1220429263@qq.com>
2018-06-01 19:53:22 +0800

    将java练习使用git管理

---
+++ src/com/lfy/CreateProcess.java
@@ -0,0 +1,31 @@
+package com.lfy;
+
+/**
+ * java创建进程
+ * @author lfy
+ * @since 2018/06/01
+ * */
+public class CreateProcess {
+
+	public static void main(String[] args) throws Exception {
+
+		//1、Create Process by Runtime's exec() mothod
+		CreateProcess temp=new CreateProcess();
+//		temp.useRuntime();
+		//2、Create Process by ProcessBuilder
+		temp.useProcessBuilder();
+	}
+	
+	private void useRuntime()throws Exception{
+		Runtime run=Runtime.getRuntime();
+		//open notepad or D:\\eclipse\\eclipse.exe
+		run.exec("notepad");
+	}
+	
+	private void useProcessBuilder()throws Exception{
+		//open notepad or D:\\eclipse\\eclipse.exe
+        ProcessBuilder pBuilder = new ProcessBuilder("notepad");
+        pBuilder.start();
+	}
+
+}
