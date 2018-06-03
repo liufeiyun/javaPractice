dc9c7d8c3b4aee701bf35ea2ab422cee4a90a33d
fyliu <1220429263@qq.com>
2018-06-03 12:38:28 +0800

    创建简单的测试以及一些常用的注解解析与测试

--- src/com/lfy/Junit/CalculatorJunitTest1.java
+++ src/com/lfy/Junit/CalculatorJunitTest1.java
@@ -1,16 +1,25 @@
 package com.lfy.Junit;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * 最简单的测试类*/
-public class CalculatorJunitTest {
+ * 使用更多的注解简写代码
+ * 更多注解描述见JunitTestAnnotation.java
+ * */
+public class CalculatorJunitTest1 {
 
+	Calculator c = null;
+	
+	@Before
+	public void testBeforeClass(){
+		c = new Calculator();
+	}
+	
 	@Test
     //测试 add()方法
     public void testAdd(){
-        Calculator c = new Calculator();
         int result = c.add(1, 2);
         Assert.assertEquals(result, 3);
     }
@@ -18,7 +27,6 @@ public class CalculatorJunitTest {
     @Test
     //测试 sub()方法
     public void testSub(){
-        Calculator c = new Calculator();
         int result = c.sub(2, 1);
         Assert.assertEquals(result, 1);
     }
